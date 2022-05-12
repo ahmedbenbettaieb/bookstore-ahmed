1484d9a0fbe4ad7ce547c3064cd98aeb4bf69189
ahmedbenbettaieb <ahmedbenbettaieb@isimg.tn>
2022-05-12 11:32:33 +0100

    restoring proj

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -82,7 +82,7 @@ namespace Bookstore.Controllers
             }
         }
 
-        // GET: Author/Delete/6
+        // GET: Author/Delete/5
         public ActionResult Delete(int id)
         {
             var author = authorRepository.Find(id);
